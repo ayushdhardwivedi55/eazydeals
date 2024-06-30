552ca7413a8138e6f96c79f098a3164809ce8d75
eazydeals <princedhardwivedi55@gmail.com>
2024-07-01 03:16:18 +0530

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental:{
+        serverActions: true,
+        serverComponentsExternalPackages: ['mongoose']
+    },
+    images:{
+        domains:['m.media-amazon.com']
+    }
+};
+
+export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     experimental: {
+//       serverActions: true,
+//       serverComponentsExternalPackages: ['mongoose']
+//     },
+//     images: {
+//       domains: ['m.media-amazon.com']
+//     }
+//   }
+  
+//   module.exports = nextConfig
